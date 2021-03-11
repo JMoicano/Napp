d799dbc99a703cb04f11cb8cad9dc981eb934dba
JMoicano <jmariossilva@gmail.com>
2021-03-11 20:17:30 -0300

    Image and design tweeks

--- app/src/main/java/com/jmoicano/napp/ui/login/Login.kt
+++ app/src/main/java/com/jmoicano/napp/ui/login/Login.kt
@@ -2,24 +2,29 @@ package com.jmoicano.napp.ui.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.Button
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jmoicano.napp.R
 import com.jmoicano.napp.ui.theme.NappTheme
 
 @Composable
@@ -29,17 +34,19 @@ fun LoginForm() {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
         Image(
             modifier = Modifier.size(150.dp),
-            imageVector = Icons.Default.Notifications, //Placeholder until get right image
+            painter = painterResource(id = R.drawable.ic_nvector), //Placeholder until get right image
             colorFilter = ColorFilter.tint(
                 color = LocalContentColor.current
             ),
             contentDescription = "Napp Logo",
         )
-        Column {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
             TextField(
                 value = email,
                 keyboardOptions = KeyboardOptions(
@@ -62,19 +69,27 @@ fun LoginForm() {
                 },
                 label = { Text(text = "Password") }
             )
-        }
-        TextButton(onClick = {
+            Box(
+                modifier = Modifier
+                    .padding(10.dp)
+            ) {
+                Button(
+                    modifier = Modifier.defaultMinSize(100.dp),
+                    onClick = {
 
-        }) {
-            Text(text = "Login")
+                    }
+                ) {
+                    Text(text = "Login")
+                }
+            }
         }
     }
 }
 
 @Composable
 @Preview
-fun LoginScreen(){
-    NappTheme {
+fun LoginScreen() {
+    NappTheme(darkTheme = true) {
         Scaffold(topBar = {
             TopAppBar(title = {
                 Text(text = "Napp!")
