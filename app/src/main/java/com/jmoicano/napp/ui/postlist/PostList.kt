4da92d6727b35d7e1425bcd3fa62d530af139dd2
JMoicano <jmariossilva@gmail.com>
2021-03-25 19:42:04 -0300

    Fix channel image

--- app/src/main/java/com/jmoicano/napp/ui/postlist/PostList.kt
+++ app/src/main/java/com/jmoicano/napp/ui/postlist/PostList.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -64,7 +65,8 @@ fun ChannelList() {
 @Composable
 fun ChannelImage() {
     CoilImage(
-        data = "https://picsum.photo/300/300",
+        modifier = Modifier.size(50.dp),
+        data = "https://picsum.photos/200",
         contentDescription = "",
         requestBuilder = {
             transformations(CircleCropTransformation())
